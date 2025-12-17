52af5d139415c3c5cec8f849d8f66a2014c83d1b
2502715-cpu <2502715@students.au.edu.pk>
2025-12-17 22:51:14 +0500

    Bobby.cpp

--- C++code.cpp
+++ C++code.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main() {
-    // Prints "Hello, World!" to the console
-    std::cout << "Hello, World!" << std::endl;
+    // Prints "WELCOME to UnderWorld!" to the console
+    std::cout << "Welcome to UnderWorld!" << std::endl;
     
     // Returns 0 to indicate successful execution
     return 0;
